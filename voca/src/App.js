cc2870bc4917ae98a1d83eb55db574895d88a832
richie <richie.kwon1@gmail.com>
2022-10-19 13:57:19 +0100

    init

---
+++ voca/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Component/Hello';
+import Welcome from './Component/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello></Hello>
+      <Welcome></Welcome>
+    </div >
+  );
+}
+
+export default App;
---
+++ voca/src/Component/Welcome.js
@@ -0,0 +1,3 @@
+export default function Welcome() {
+    return <h2>GoldersGreen</h2>
+}
