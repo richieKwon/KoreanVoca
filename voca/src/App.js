33c04a1ecc557f0de4b6c3b3b7a1199a7b36f6e8
richie <richie.kwon1@gmail.com>
2022-10-21 20:39:10 +0100

    route

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,9 +1,12 @@
 import DayList from "./Component/DayList"
 import Header from "./Component/Header"
+import Day from "./Component/Day"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 export default function App() {
   return <div className="App">
     <Header />
     <DayList />
+    <Day />
   </div>
 }
