be21bb5418e981d78c828c4976c3b7fa0a63cd7f
richie <richie.kwon1@gmail.com>
2022-10-25 19:28:04 +0100

    delete

--- voca/src/Component/Word.js
+++ voca/src/Component/Word.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
 
+    const [word, setWord] = useState(w);
     const [isShown, setIsShown] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone)
 
@@ -10,15 +11,45 @@ export default function Word({ word }) {
     }
 
     function toggleDone() {
-        setIsDone(!isDone)
+        // setIsDone(!isDone)
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+                    ...word,
+                    isDone: !isDone
+                })
+        })
+            .then(res => {
+                if (res.ok) {
+                    setIsDone(!isDone);
+                }
+            })
     }
+
+    function del() {
+        if (window.confirm('want to delete the word?')) {
+
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: 'DELETE',
+            }).then(res => { if (res.ok) { setWord({ id: 0 }); } });
+        }
+    }
+
+    if (word.id === 0) { return null; }
+
     return (
         <tr className={isDone ? "off" : ""}>
             <td><input type="checkbox" checked={isDone} onChange={toggleDone}></input></td>
             <td>{word.kor}</td>
             <td>{isShown && word.eng}</td>
-            <td><button onClick={toggleShow}>Meaning - {isShown ? 'Hide' : 'Display'}</button></td>
-            <td><button className="btn_del">Delete</button></td>
+            <td>
+                <button onClick={toggleShow}>{isShown ? 'Hide' : 'Display'}</button>
+                <button onClick={del} className="btn_del">Delete</button>
+            </td>
         </tr>
     );
 }
