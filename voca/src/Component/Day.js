c6e583bdb3a14f27c0783da0ed4a022337f8b6f4
richie <richie.kwon1@gmail.com>
2022-10-25 18:22:42 +0100

    useeffect

--- voca/src/Component/Day.js
+++ voca/src/Component/Day.js
@@ -1,6 +1,8 @@
-import dummmy from "./db/data.json"
+// import dummmy from "./db/data.json"
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+import useFetch from './hooks/useFetch';
 
 export default function Day() {
 
@@ -8,17 +10,27 @@ export default function Day() {
 
     const { day } = useParams();
 
-    const wordList = dummmy.words.filter(
-        word => (word.day === Number(day))
-    );
+    // const [words, setWords] = useState([]);
+
+    // const wordList = dummmy.words.filter(
+    //     word => (word.day === Number(day))
+    // );
 
     // console.log(a);
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:3001/words?day=${day}`)
+    //         .then(res => { return res.json() })
+    //         .then(data => { setWords(data) });
+    // }, [day])
+
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return <>
         <h2>Day - {day}</h2>
         <table>
             <tbody>
-                {wordList.map(
+                {words.map(
                     word => (
                         <Word word={word} key={word.id} />
                     )
--- voca/src/Component/DayList.js
+++ voca/src/Component/DayList.js
@@ -1,14 +1,25 @@
-
-import dummy from "./db/data.json"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import useFetch from './hooks/useFetch';
 
 export default function DayList() {
-    console.log(dummy);
+
+    const days = useFetch("http://localhost:3001/days");
+    // const [days, setDays] = useState([]);
+
+    // useEffect(() => {
+    //     fetch("http://localhost:3001/days")
+    //         .then(res => { return res.json() })
+    //         .then(data => { setDays(data) });
+    // }, []);
+
+
     return <>
         <ul className="list_day">
-            {dummy.days.map(day => <li key={day.id}>
+            {days.map(day => <li key={day.id}>
                 <Link to={`/day/${day.day}`}>Day: {day.day} </Link>
             </li>)}
         </ul>
+
     </>
 }
